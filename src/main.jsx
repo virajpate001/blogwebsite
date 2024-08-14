c531713f3cfd5892fe5b9f771d532c10ce9f98aa
virajpate001 <virajpate001@gmail.com>
2024-08-14 21:56:23 +0530

    installation done

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import  ReactDOM  from 'react-dom/client'
+import App from './App.jsx'
+import React from 'react'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Home from './pages/Home.jsx';
+
+
+const router = createBrowserRouter([{
+  path:"/",
+  element:<App/>,
+  children:[
+    {
+      path:"/",
+      element:<Home/>
+    }
+  ]
+}])
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+   
+  <React.StrictMode>
+   
+    <RouterProvider router={router}/>
+  </React.StrictMode>
 )
+
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Home</h1>
+      <p className=' text-lg font-semibold text-yellow-400'>This os test</p>
+    </div>
+  )
+}
+
+export default Home
