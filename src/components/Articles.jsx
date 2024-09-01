f46056f3c4daf1318fd3e31bc94c3b2c727fa09a
virajpate001 <virajpate001@gmail.com>
2024-09-01 10:27:10 +0530

    blog card added

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { fetchArticles, fetchArticlespagination } from "../services/api";
 import { Link } from "react-router-dom";
 import Pagination from "./Pagination";
+import BlogCard from "./BlogCard";
 
 function Articles() {
   const [articles, setArticle] = useState([]);
@@ -39,41 +40,7 @@ function Articles() {
   return (
     <>
       <div className="m-auto w-11/12 ">
-        <ul className=" grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4">
-          {articles.map((article, index) => {
-            return (
-              <li key={index}>
-                <div className="rounded-xl shadow-md">
-                  <div className="relative">
-                    <Link to={`/post/${article.attributes.slug}`}>
-                      <img
-                        src={article.attributes.profile_img.data.attributes.url}
-                        alt={article.attributes.Title}
-                        className="w-full h-60 aspect-square object-cover"
-                      />
-                    </Link>
-                  </div>
-                  <Link to={`/post/${article.attributes.slug}`}>
-                    <div className="px-4 py-6">
-                      <div className="mb-2 flex justify-between">
-                        <p className="name">Viraj Pate</p>
-                        <p className="date text-sm text-gray-500">
-                          May 20, 2021
-                        </p>
-                      </div>
-                      <div>
-                        <h2 className="text-lg font-medium leading-5">
-                          {article.attributes.Title}
-                          ...
-                        </h2>
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        <BlogCard articles={articles} />
 
         <Pagination
           onPageChange={onPageChange}
--- src/pages/TagsPage.jsx
+++ src/pages/TagsPage.jsx
@@ -4,6 +4,7 @@ import { fetchSingleTag } from "../services/api";
 import Breadcrumb from "../components/MyBreadcrumb";
 import axios from "axios";
 import Filter from "../components/Filter";
+import BlogCard from "../components/BlogCard";
 
 const TagsPage = () => {
   const { slug } = useParams();
@@ -41,41 +42,7 @@ const TagsPage = () => {
       <Filter />
 
       <div className="m-auto w-11/12 ">
-        <ul className=" grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-4">
-          {articles.map((article, index) => {
-            return (
-              <li key={index}>
-                <div className="rounded-xl shadow-md">
-                  <div className="relative">
-                    <Link to={`/post/${article.attributes.slug}`}>
-                      <img
-                        src={article.attributes.profile_img.data.attributes.url}
-                        alt={article.attributes.Title}
-                        className="w-full h-60 aspect-square object-cover"
-                      />
-                    </Link>
-                  </div>
-                  <Link to={`/post/${article.attributes.slug}`}>
-                    <div className="px-4 py-6">
-                      <div className="mb-2 flex justify-between">
-                        <p className="name">Viraj Pate</p>
-                        <p className="date text-sm text-gray-500">
-                          May 20, 2021
-                        </p>
-                      </div>
-                      <div>
-                        <h2 className="text-lg font-medium leading-5">
-                          {article.attributes.Title}
-                          ...
-                        </h2>
-                      </div>
-                    </div>
-                  </Link>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        <BlogCard articles={articles} />
       </div>
     </>
   );
