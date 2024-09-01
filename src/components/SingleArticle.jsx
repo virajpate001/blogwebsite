9801f8f7ec8b302a2c3ee4bb39e39eeb5b39d67e
virajpate001 <virajpate001@gmail.com>
2024-09-01 11:50:45 +0530

    sidebar done

--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,6 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { fetchSingleArticle } from "../services/api";
+import {
+  fetchCategories,
+  fetchRecentArticles,
+  fetchSingleArticle,
+} from "../services/api";
 import { Link, useParams } from "react-router-dom";
+import RecentPost from "./RecentPost";
 
 function SingleArticle() {
   const { slug } = useParams();
@@ -8,6 +13,8 @@ function SingleArticle() {
   const [showcategories, setShowCategorie] = useState([]);
   const [showTags, setShowTags] = useState([]);
   const [error, setError] = useState("");
+  const [rPost, setRpost] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     const getsArticle = async () => {
@@ -19,7 +26,28 @@ function SingleArticle() {
       }
     };
 
+    const getRecentArticle = async () => {
+      try {
+        const getRecentArt = await fetchRecentArticles();
+        setRpost(getRecentArt);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    const getCategories = async () => {
+      try {
+        const getCategory = await fetchCategories();
+
+        setCategories(getCategory);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
     getsArticle();
+    getRecentArticle();
+    getCategories();
   }, [slug]);
 
   useEffect(() => {
@@ -40,7 +68,6 @@ function SingleArticle() {
 
   return (
     <>
-      <div>{console.log(singleArticle)}</div>
       <div className="w-full px-4 py-4">
         <div className="m-auto w-full bg-cover text-center text-white">
           <img
@@ -51,77 +78,84 @@ function SingleArticle() {
         </div>
       </div>
 
-      <div className="m-auto w-4/5 my-4">
-        <div className="category mb-8">
-          <div className="mb-2 flex justify-between gap-4 items-center">
-            {showcategories.length === 0 ? (
-              <ul>
-                <li className="rounded-3xl bg-yellow-400 px-4 py-2 text-sm">
-                  unauthorized
-                </li>
-              </ul>
-            ) : (
-              <ul className="flex gap-2">
-                {showcategories.map((catlist) => (
-                  <li key={catlist.id}>
-                    <Link
-                      to={`/category/${catlist.attributes.slug}`}
-                      className="rounded-3xl bg-yellow-400 px-4 py-2 text-sm"
-                    >
-                      {catlist.attributes.Name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
+      <div className="m-auto w-11/12 my-4">
+        <div className="flex flex-wrap gap-14">
+          <div className="w-full sm:w-3/4">
+            <div className="category mb-8">
+              <div className="mb-2 flex justify-between gap-4 items-center">
+                {showcategories.length === 0 ? (
+                  <ul>
+                    <li className="rounded-3xl bg-yellow-400 px-4 py-2 text-sm">
+                      unauthorized
+                    </li>
+                  </ul>
+                ) : (
+                  <ul className="flex gap-2">
+                    {showcategories.map((catlist) => (
+                      <li key={catlist.id}>
+                        <Link
+                          to={`/category/${catlist.attributes.slug}`}
+                          className="rounded-3xl bg-yellow-400 px-4 py-2 text-sm"
+                        >
+                          {catlist.attributes.Name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
 
-            <div className="flex gap-4">
-              <p className="name">
-                {singleArticle.attributes.author.data.attributes.Name}
-              </p>
-              {/* <p className="date text-sm text-gray-500">May 20, 2021</p> */}
-              {/* {singleArticle.attributes.createdAt} */}
+                <div className="flex gap-4">
+                  <p className="name">
+                    {singleArticle.attributes.author.data.attributes.Name}
+                  </p>
+                  {/* <p className="date text-sm text-gray-500">May 20, 2021</p> */}
+                  {/* {singleArticle.attributes.createdAt} */}
 
-              {new Date(
-                singleArticle.attributes.publishedAt
-              ).toLocaleDateString("en-US", {
-                month: "long", // Full month name
-                day: "numeric", // Numeric day
-                year: "numeric", // Full year
-              })}
+                  {new Date(
+                    singleArticle.attributes.publishedAt
+                  ).toLocaleDateString("en-US", {
+                    month: "long", // Full month name
+                    day: "numeric", // Numeric day
+                    year: "numeric", // Full year
+                  })}
+                </div>
+              </div>
+            </div>
+            <h1 className="text-2xl sm:text-3xl font-semibold my-3">
+              {singleArticle.attributes.Title}
+            </h1>
+            <div className="w-full border-b border-neutral-200 dark:border-neutral-700"></div>
+            <div className="text-neutral-500 py-4">
+              {singleArticle.attributes.Description}
+            </div>
+
+            <div className="tags mt-4">
+              <div className="mb-2 flex justify-between gap-4 items-center">
+                {showTags.length === 0 ? (
+                  <ul>
+                    <li className="rounded-3xl bg-yellow-400 px-4 py-2 text-sm">
+                      notag
+                    </li>
+                  </ul>
+                ) : (
+                  <ul className="flex gap-2">
+                    {showTags.map((taglist) => (
+                      <li key={taglist.id}>
+                        <Link
+                          to={`/tags/${taglist.attributes.slug}`}
+                          className="rounded-xl bg-white hover:bg-neutral-50 px-4 py-2 text-sm"
+                        >
+                          {taglist.attributes.Name}
+                        </Link>
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
             </div>
           </div>
-        </div>
-        <h1 className="text-2xl sm:text-3xl font-semibold my-3">
-          {singleArticle.attributes.Title}
-        </h1>
-        <div className="w-full border-b border-neutral-200 dark:border-neutral-700"></div>
-        <div className="text-neutral-500 py-4">
-          {singleArticle.attributes.Description}
-        </div>
-
-        <div className="tags mt-4">
-          <div className="mb-2 flex justify-between gap-4 items-center">
-            {showTags.length === 0 ? (
-              <ul>
-                <li className="rounded-3xl bg-yellow-400 px-4 py-2 text-sm">
-                  notag
-                </li>
-              </ul>
-            ) : (
-              <ul className="flex gap-2">
-                {showTags.map((taglist) => (
-                  <li key={taglist.id}>
-                    <Link
-                      to={`/tags/${taglist.attributes.slug}`}
-                      className="rounded-xl bg-white hover:bg-neutral-50 px-4 py-2 text-sm"
-                    >
-                      {taglist.attributes.Name}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            )}
+          <div className="w-full sm:w-1/5">
+            <RecentPost rPost={rPost} categories={categories} />
           </div>
         </div>
       </div>
